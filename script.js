3887acb22f98f6aacff6a671c8c8eb27eaa40fc8
jackhelvetica <ng.jinghan20@gmail.com>
2017-12-21 17:00:33 +0800

    update of website

--- script.js
+++ script.js
@@ -45,4 +45,38 @@ $('[id^=carousel-selector-]').click( function(){
         });
        
       });
+      $("#hide").click(function(){
+        $("p").hide(1000);
+    });
+    
+    $("#show").click(function(){
+        $("p").show(1000);
+    });
+    function openCity(evt, cityName) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+    
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+    
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+    
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    $("#test").stop().animate({opacity: 0},1000,function(){
+        $(this).css({'background-image': "url('pic 1.jpg')"})
+                   .animate({opacity: 1},{duration:1000});
+     });
+
+     
+    
     
